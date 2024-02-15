dced1f93ca9d918ff1c94cfbb8ad5d86f4adf74e
Nikki0830 <gniki0830@gmail.com>
2024-02-15 12:59:32 +0530

    Big-O notation

---
+++ DataStructure/Primeno.js
@@ -0,0 +1,34 @@
+// const PrimeNo = (num) =>{
+//     if(nu)
+// }
+
+// PrimeNo(3)
+// let num = 4
+// let result = Math.sqrt(num)
+// console.log(result)
+
+//sum of indeces of targeteted value
+
+let num = [2, 3];
+let target = 5;
+
+for(let i = 0; i < num.length; i++){
+        console.log(num[i],[i] )
+        // console.log([i+1])
+    if(num[i] + [i+1] === target){
+          
+        console.log([i,i+1])
+        
+    }
+
+}
+
+// function SumTraget() {
+//   for (let i = 0; i < num.length; i++) {
+//     //num[i] 2 + (i=0+1=1) num[3] + (i=1+1)
+//     if (num[i] + (i + 1) === target) {
+//       return [i,i+1]
+//     }
+//   }
+// }
+// console.log(SumTraget());
